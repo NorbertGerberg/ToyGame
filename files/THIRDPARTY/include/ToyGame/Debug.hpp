6528a28a844f449f14b06eae441abe1b87c38477
Norbert <61798764+ENGY-ORG@users.noreply.github.com>
2024-03-04 21:31:46 +0100

    small change

--- files/THIRDPARTY/include/ToyGame/Debug.hpp
+++
@@ -1,9 +0,0 @@
-/*=========================================
-	Copyright 2024-2025 Norbert Gerberg
-=========================================*/
-#ifndef DEBUG_HPP
-#define DEBUG_HPP
-
-#define TG_ASSERT(vl) assert(vl)
-#define TG_DEBUGBREAK __debugbreak()
-#endif
